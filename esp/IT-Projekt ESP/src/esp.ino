99bec02434a2670a34c9fd11e913f8b9fea9f569
Trainee02 <noahcasati@hotmail.de>
2022-01-09 02:01:20 +0100

    Update ESP_WiFi Module

--- esp/IT-Projekt ESP/src/esp.ino
+++ esp/IT-Projekt ESP/src/esp.ino
@@ -1,42 +1,95 @@
-void initWiFi() 
+#include "WiFi.h"
+#include "SoftwareSerial.h"
+
+const char ssid[]     = "UPCC434158";
+const char password[] = "xxbj5wpkmzcA";
+const char* host = "localhost";
+
+int status = WL_IDLE_STATUS;
+
+SoftwareSerial esp_uno (7 , 3); // RX, TX
+
+void setup()
 {
- WiFi.mode(WIFI_STA);
- WiFi.begin(Name des WLAN, PassWort); 
- Serial.print("Connecting to WiFi ..");
- while (WiFi.status() != WL_CONNECTED)
- {
- Serial.print('.');
- delay(1000);
- }
- Serial.println(WiFi.localIP());
+  
+    Serial.begin(9600);
+    esp_uno.begin(9600);
+
+    // Connect Wifi
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    status = WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
 }
+
+
+
+void loop()
+{
+  
+float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();  
+  if(isnan(temperature) || isnan(humidity)){
+    Serial.println("Failed to read DHT11");
+  }else{
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" *C");
+    delay(3000);
+  }
+   
+    Serial.print("connecting to ");
+    Serial.println(host);
+
+    // Use WiFiClient class to create TCP connections
+    WiFiClient client;
+    const int httpPort = 5555;
+    if (!client.connect(host, httpPort)) {
+        Serial.println("connection failed");
+        return;
+    }
+
  
-void setup() {
- Serial.begin(115200);
- initWiFi();
- Serial.print("RRSI: ");
- Serial.println(WiFi.RSSI());
-}
-void loop() {
- dtemperature = analogRead(34);
-*******************************************
- if (client.connect(server, 1234))
- {
- Serial.println("Connected, sending data...");
- client.print("GET phpmyadmin/InfoPushing/temprecord.php?"); 
- client.print("Temperature=");
- client.print(dtemperature);
- client.println(" HTTP/1.1"); 
- client.println("Host: 192.168.0.104"); 
- client.println("Connection: close");
- client.println();
- client.println();
- delay (5000);
- client.stop();
- Serial.println("Finished sending data");
-}
-else
-{ Serial.println("Connection has failed\n"); }
-delay (3000);
-**************************************************************
+
+
+    // This will send the request to the server
+ client.print(String("GET http://your_hostname/iot_project/connect.php?") + 
+                          ("&temperature=") + temperature +
+                          ("&humidity=") + humidity +
+                          " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n\r\n");
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 1000) {
+            Serial.println(">>> Client Timeout !");
+            client.stop();
+            return;
+        }
+    }
+
+    // Read all the lines of the reply from server and print them to Serial
+    while(client.available()) {
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+        
+    }
+
+    Serial.println();
+    Serial.println("closing connection");
 }
