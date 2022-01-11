87f80874ad2e1e01bbbdd89c1ca5630bf942ccdd
Trainee02 <noahcasati@hotmail.de>
2022-01-11 13:18:21 +0100

    update

--- esp/IT-Projekt ESP/src/connect.php
+++ esp/IT-Projekt ESP/src/connect.php
@@ -3,34 +3,35 @@
 $dbname = 'IT-Projekt-DB';
 $dbuser = 'admin';  
 $dbpass = 'admin'; 
-$dbhost = 'http://192.168.0.228/'; 
+$dbhost = 'localhost'; 
 
-$connect = @mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
 
-if(!$connect){
-	echo "Error: " . mysqli_connect_error();
-	exit();
+$sql = "INSERT INTO heizung (state, time) VALUES ('$_GET[heizung]', CURRENT_TIMESTAMP)";
+$sql = "INSERT INTO pumpe (state, time) VALUES ('$_GET[pumpe]', CURRENT_TIMESTAMP)";
+$sql = "INSERT INTO rpm (value, time) VALUES ('$_GET[rpm]', CURRENT_TIMESTAMP)";
+//$sql = "UPDATE steuerung SET abpumpen, time, ) VALUES ('$_GET[pumpe]', CURRENT_TIMESTAMP) WHERE id";
+$sql = "INSERT INTO temperature (value, time) VALUES ('$_GET[temperature]', CURRENT_TIMESTAMP)";
+$sql = "INSERT INTO tuer (state, time) VALUES ('$_GET[tuer]', CURRENT_TIMESTAMP)";
+$sql = "INSERT INTO waschmittel (value, time) VALUES ('$_GET[waschmittel]', CURRENT_TIMESTAMP)";
+$sql = "INSERT INTO waterlevel (value, time) VALUES ('$_GET[waterlevel]', CURRENT_TIMESTAMP)";
+$sql = "INSERT INTO watervalve (state, time) VALUES ('$_GET[watervalve]', CURRENT_TIMESTAMP)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-echo "Connection Success!<br><br>";
+$conn->close();
 
-$temp = $_GET["temperature"];
-$door = ;
-$heizung =;
-$pumpe =;
-$watervalve =;
+echo "klappt\n\n";
 
-int rpm = 0;
-int waschmittel = 0;
-
-double temp = 0.0;
-double waterlevel = 0.0;
-
-
-$query = "INSERT INTO IT-Projekt-DB.heizung (state) VALUES ('$heizung')";
-$query = "INSERT INTO IT-Projekt-DB.pumpe (state) VALUES ('$pumpe')";
-$result = mysqli_query($connect,$query);
-
-echo "Insertion Success!<br>";
+echo $_GET["heizung"];
 
 ?>
